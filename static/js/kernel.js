974e721a191b6f3e7dfd36e9e32151d2534ff3b0
Michael W <baddaemon87@gmail.com>
2017-03-14 11:23:05 +0100

    cve_tracker: Inline editing of comments
    * Allow inline-editing of comments to make it easier for maintainers to
      share their knowledge
    * Use the same placeholder string wherever notes are editable
    
    Change-Id: I286043a5ac462db4d9b71e557bc63a4a7828691b

--- static/js/kernel.js
+++ static/js/kernel.js
@@ -1,5 +1,6 @@
 function openLinks(cve, cve_id) {
-  $("#links").empty();
+  $("#cvelinks").text("Loading...");
+  $("#cvenotes").text("Loading...");
 
   $.ajax({
     'type': 'POST',
@@ -9,12 +10,13 @@ function openLinks(cve, cve_id) {
              cve_id: cve_id,
             })
   }).done(function(data) {
+    $("#cvelinks").empty();
     $.each(JSON.parse(data), function(i, v) {
       if (!v.desc) v.desc = 'No description';
-      $("#links").append("<a href='" + v.link + "' target='_blank' >" + v.link + "</a> - " + v.desc + "<br/>");
+      $("#cvelinks").append("<a href='" + v.link + "' target='_blank' >" + v.link + "</a> - " + v.desc + "<br/>");
     });
-    $("#links").append("<br><a href='/editcve/" + cve + "'>edit</a>");
-    $("#links").dialog('option', 'title', cve).dialog('open');
+    $("#cveeditlink").attr("href", "/editcve/" + cve);
+    $("#cveinfodialog").dialog('option', 'title', cve).dialog('open');
   });
 
   $.ajax({
@@ -26,15 +28,48 @@ function openLinks(cve, cve_id) {
     })
   }).done(function(data) {
     data = JSON.parse(data);
+    $('#cvenotes_input').val(data[0].notes);
     if (!data[0].notes) {
       data[0].notes = 'No notes';
     }
-    $("#links").prepend("<div class='notes'>" + data[0].notes + "</div>");
+    $("#cvenotes").text(data[0].notes);
+    $('#editnotesdialog').attr('cve_id', cve_id);
   });
 }
 
 $(document).ready(function() {
-  $("#links").dialog({ autoOpen: false,
-                       width: 'auto'
-                     });
+  $("#cveinfodialog").dialog({ autoOpen: false, width: 'auto' });
+  $("#editnotesdialog").dialog({ autoOpen: false, width: 'auto' });
+
+  $("#cveinfodialog").on('dialogbeforeclose', function(event, ui) {
+    $("#editnotesdialog").dialog('close');
+  });
+
+  $('#savenoteslink').on('click', function() {
+    var cve_id = $('#editnotesdialog').attr('cve_id');
+    var notes = $('#cvenotes_input').val();
+    $.ajax({
+      'type': 'POST',
+      'url': '/editnotes',
+      'contentType': 'application/json',
+      'data': JSON.stringify({
+               cve_id: cve_id,
+               cve_notes: notes,
+      })
+    }).done(function(data) {
+      if (data.error == "success") {
+        if (!notes) {
+          notes = 'No notes';
+        }
+        $('#cvenotes').text(notes);
+        $('#editnotesdialog').dialog('close');
+      } else {
+        $("#editnoteserror").empty().append(data.error);
+      }
+    });
+  });
 });
+
+function editnotes() {
+  $('#editnotesdialog').dialog('option', 'title', 'Edit CVE notes').dialog('open');
+}
